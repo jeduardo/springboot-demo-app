68b55eabca2e7695860393eb036666ba92a59c51
J. Eduardo <j.eduardo@gmail.com>
2026-08-19 23:12:06 +0100

    test: keep Flyway in control of test schema

--- src/test/java/org/jeduardo/entries/ProductionPropertiesTest.java
+++ src/test/java/org/jeduardo/entries/ProductionPropertiesTest.java
@@ -12,14 +12,22 @@ import org.junit.jupiter.api.Test;
 class ProductionPropertiesTest {
 
     @Test
-    void productionConfigurationKeepsFlywayInControlAndExposesOnlySafeEndpoints() throws Exception {
-        Properties properties = new Properties();
-        try (Reader reader = Files.newBufferedReader(Path.of("src/main/resources/application.properties"))) {
-            properties.load(reader);
-        }
+    void applicationConfigurationsKeepFlywayInControlAndProductionExposesOnlySafeEndpoints() throws Exception {
+        Properties productionProperties = loadProperties("src/main/resources/application.properties");
+        Properties testProperties = loadProperties("src/test/resources/application.properties");
 
-        assertThat(properties)
+        assertThat(productionProperties)
                 .containsEntry("spring.jpa.hibernate.ddl-auto", "validate")
                 .containsEntry("management.endpoints.web.exposure.include", "health,prometheus");
+        assertThat(testProperties)
+                .containsEntry("spring.jpa.hibernate.ddl-auto", "validate");
+    }
+
+    private Properties loadProperties(String path) throws Exception {
+        Properties properties = new Properties();
+        try (Reader reader = Files.newBufferedReader(Path.of(path))) {
+            properties.load(reader);
+        }
+        return properties;
     }
 }
